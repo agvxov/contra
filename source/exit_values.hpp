88b0db5fca6aa209657d0ea99ecbb47135502f38
anon <anon@anon.anon>
2023-11-18 02:15:41 +0100

    clearer tag mismatch exit conditions

--- source/exit_values.hpp
+++ source/exit_values.hpp
@@ -4,4 +4,6 @@ enum {
 	IO_ERROR        = 2,
 	UNKNOWN_SET     = 3,
 	POPULATED_STACK = 4,	// most likely signals that more tags were opened than closed, ie the user forgot adding a '}' somewhere
+	TAG_NOT_NAMED   = 5,
+	TAG_NOT_FOUND   = 6,
 };
